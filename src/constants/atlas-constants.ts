02a5d3962e74ef891f45038fb28f5411df230f5a
Jonathan Ioffe <jonathan_ioffe@apple.com>
2020-05-14 15:30:30 +0300

    wip

---
+++ src/constants/atlas-constants.ts
@@ -0,0 +1,10 @@
+export const AtlasServerAddress = 'ATLAS_WS_ADDRESS'
+export const MessageClassName = {
+  UserAuthRequest: 'tau.atlas.connections.UserAuthenticationRequest',
+  UserAuthResponse: 'tau.atlas.connections.UserAuthenticationResponse',
+  Connection: 'tau.atlas.messages.ConsumerConnectionStateExtended',
+  Localization: 'tau.atlas.messages.LocalizationMessage',
+  Detection: 'tau.atlas.messages.DetectionMessage',
+  TagSummary: 'tau.atlas.messages.TagSummaryMessage',
+  SystemStructure: 'tau.atlas.messages.SystemStructureMessageExtended',
+}
