2c10bd7001abeabcee6d44bed9f4e186b97f7abf
Jonathan Ioffe <jonathan_ioffe@apple.com>
2020-05-14 11:28:44 +0300

    wip

---
+++ src/constants/app-constants.ts
@@ -0,0 +1,6 @@
+export const AppName = 'ATLAS Web App'
+export const CookieName = 'atlas-google-api-cookie'
+export const GoogleApiClientId = 'GOOGLE_API_CLIENT_ID'
+export const NumOfBasestationsPerTag = 5
+export const NumOfLocalizationsPerTag = 100
+export const MapUpdateIntervalSeconds = 10
